91cbded7b499a52c1917097978cbc3baedb8968a
PedamalluAbhiram <ugs207145_it.abhiram@cbit.org.in>
2023-07-03 11:55:44 +0530

    init

---
+++ client/src/Homepage.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Homepage.css';
+
+function Homepage() {
+  return (
+    <div>
+        <div className='bg1'>
+        <img src='bg.webp'/>
+        <div className='text-overlay'>
+        <img className='overlay-image' src='templogo.png' alt='Overlay' />
+
+          <h1>Welcome to Terrace Gardeners</h1>
+
+        </div>
+        </div>
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Homepage
